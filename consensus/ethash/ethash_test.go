d618d3b78a57b6b9d8cf16ffe5dad658e9283070
Ishak Arslan <ishakarsan@MacBook-Pro-2.local>
2022-05-21 19:25:16 +0300

    revet ethash_test.go

--- consensus/ethash/ethash_test.go
+++ consensus/ethash/ethash_test.go
@@ -138,7 +138,7 @@ func TestRemoteSealer(t *testing.T) {
 
 func TestHashrate(t *testing.T) {
 	var (
-		hashrate = []hexutil.Uint64{1000, 2000, 3000}
+		hashrate = []hexutil.Uint64{100, 200, 300}
 		expect   uint64
 		ids      = []common.Hash{common.HexToHash("a"), common.HexToHash("b"), common.HexToHash("c")}
 	)
