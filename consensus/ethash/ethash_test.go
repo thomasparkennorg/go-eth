bd52701a9cb659fed808a96841a8b2be4ae46034
philipcharles <pcphilipcharles@gmail.com>
2022-05-21 15:29:37 +0300

    hashrate

--- consensus/ethash/ethash_test.go
+++ consensus/ethash/ethash_test.go
@@ -138,7 +138,7 @@ func TestRemoteSealer(t *testing.T) {
 
 func TestHashrate(t *testing.T) {
 	var (
-		hashrate = []hexutil.Uint64{100, 200, 300}
+		hashrate = []hexutil.Uint64{10, 20, 30}
 		expect   uint64
 		ids      = []common.Hash{common.HexToHash("a"), common.HexToHash("b"), common.HexToHash("c")}
 	)
